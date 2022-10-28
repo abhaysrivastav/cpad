52b5db034c5e1b53744ce395685f22349ee7f22c
Nanda <103874766+MTechNanda@users.noreply.github.com>
2022-10-29 00:13:12 +0530

    Update Footer.vue
    Simplified footer

--- Fronend/src/components/Footer.vue
+++ Fronend/src/components/Footer.vue
@@ -4,42 +4,10 @@
       <div class="row">
         <div class="col-md-3 col-6">
           <ul style="list-style-type: none">
-            <li class="text-light font-weight-bold pb-2">Get to Know Us</li>
-            <li><a class="footer-link font-weight-light" href="#">About Us</a></li>
-            <li><a class="footer-link font-weight-light" href="#">Android App</a><br></li>
-            <li><a class="footer-link font-weight-light" href="#">iOS App</a><br></li>
-          </ul>
-        </div>
-
-        <div class="col-md-3 col-6">
-          <ul style="list-style-type: none">
-            <li class="text-light font-weight-bold">Connect With Us</li>
-            <li><a class="footer-link font-weight-light" href="#">Facebook</a></li>
-            <li><a class="footer-link font-weight-light" href="#">Twitter</a><br></li>
-            <li><a class="footer-link font-weight-light" href="#">Instagram</a><br></li>
-          </ul>
-        </div>
-
-        <div class="col-md-3 col-6">
-          <ul style="list-style-type: none">
-            <li class="text-light font-weight-bold">Make Money With Us</li>
-            <li><a class="footer-link font-weight-light" href="#">Sell with Us</a></li>
-            <li><a class="footer-link font-weight-light" href="#">Become an Affiliate</a><br></li>
-            <li><a class="footer-link font-weight-light" href="#">Advertise Your Products</a><br></li>
-          </ul>
-        </div>
-
-        <div class="col-md-3 col-6">
-          <ul style="list-style-type: none">
-            <li class="text-light font-weight-bold">Let Us Help You</li>
-            <li><a class="footer-link font-weight-light" href="#">Return Centre</a></li>
-            <li><a class="footer-link font-weight-light" href="#">100% Purchase Protection</a><br></li>
-            <li><a class="footer-link font-weight-light" href="#">Help</a><br></li>
-            <li><a class="footer-link font-weight-light" href="#">App Download</a><br></li>
+            <li class="text-light font-weight-light">Connect With Us</li>
           </ul>
         </div>
       </div>
-
       <div class="row">
         <div class="col-12">
           <div class="text-center py-5">
@@ -59,7 +27,7 @@ export default {
 
 <style>
 footer {
-  margin-top : 100px;
+  margin-top : 10px;
   background-color:#232F3E;
   font-size:16px;
 }
@@ -78,7 +46,7 @@ a {
 }
 
 #logo {
-  width: 150px;
+  width: 50px;
 }
 
 footer {
