616ba61fc168ce54f210f914a90621785ccfeede
Nanda <103874766+MTechNanda@users.noreply.github.com>
2022-10-29 00:11:42 +0530

    Update ProductBox.vue
    Rupee symbol updated

--- Fronend/src/components/Product/ProductBox.vue
+++ Fronend/src/components/Product/ProductBox.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body">
       <router-link :to="{ name: 'ShowDetails', params: { id : product.id } }"><h5 class="card-title">{{product.name}}</h5></router-link>
-      <p class="card-text"><sup>$</sup>{{product.price}}</p>
+      <p class="card-text"><sup>₹</sup>{{product.price}}</p>
       <p class="card-text font-italic">{{product.description.substring(0,65)}}...</p>
     </div>
   </div>
